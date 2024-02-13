9c8abc4648fb780f7eb7f05757036e209c252e97
GP <omer.osama07@gmail.com>
2024-02-13 18:54:37 +0200

    Project Sample

--- app/src/main/java/com/example/loginscreen/setting/SettingActivity.kt
+++ app/src/main/java/com/example/loginscreen/setting/SettingActivity.kt
@@ -15,48 +15,10 @@ import com.facebook.login.LoginManager
 
 class SettingActivity : AppCompatActivity() {
 
-
     private lateinit var binding:ActivitySettingBinding
-    private lateinit var fbImg: ImageView
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        fbImg = findViewById(R.id.fbImg)
-
-        val accessToken = AccessToken.getCurrentAccessToken()
-
-        val request = GraphRequest.newMeRequest(accessToken) { obj, _ ->
-
-            val id = obj?.getString("id")
-            val fullName = obj?.getString("name")
-            val email = obj?.getString("email")
-            val profileUrl = obj?.getJSONObject("picture")
-                ?.getJSONObject("data")?.getString("url")
-
-            binding.fbName.text = fullName
-            binding.fbEmail.text = email
-            binding.fbId.text = id
-
-            Glide.with(applicationContext).load(profileUrl).into(fbImg)
-        }
-
-        val parameters = Bundle()
-
-        parameters.putString("fields","id,name,link,picture.type(large),email")
-
-        request.parameters = parameters
-        request.executeAsync()
-
-        binding.fbLogOut.setOnClickListener {
-
-            LoginManager.getInstance().logOut()
-
-            startActivity(Intent(this,SignInActivity::class.java))
-            finish()
-        }
     }
 }
